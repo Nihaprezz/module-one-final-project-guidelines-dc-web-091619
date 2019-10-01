3439f4e82a28780ed866721baab277ba564af313
nihaprezz <sergio13prez@gmail.com>
2019-10-01 12:53:42 -0400

    user login method created

--- bin/run.rb
+++ bin/run.rb
@@ -3,7 +3,11 @@ require_relative "../app/models/player.rb"
 require_relative "../app/models/team.rb"
 require_relative "../app/models/user_team.rb"
 require_relative "../app/models/user.rb"
+require_relative "./cli_methods.rb"
 
 
-binding.pry
-puts "HELLO WORLD"
+puts "Welcome to Sports CLI App"
+puts "-------------------------"
+puts "Once logged in type Menu for a list of commands"
+
+user_login
