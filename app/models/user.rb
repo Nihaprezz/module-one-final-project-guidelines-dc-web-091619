ed726869992ed1955ed82bcae0aa158d87029cbe
chungcha <charlieschung@gmail.com>
2019-10-01 14:15:41 -0400

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
+require "pry"
+
 class User < ActiveRecord::Base
     has_many :user_teams
     has_many :teams, through: :user_teams
 
+    
 
-
-    def add_team
-        User_team.create(user_id:self.id,team_id:team.id)
+    def add_team(team)
+        User_team.create(user_id:self.id,team_id:team)
     end
 
 
