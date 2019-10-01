2d713387385e54094f58c5288eec6fa65769ea71
chungcha <charlieschung@gmail.com>
2019-10-01 15:56:01 -0400

    made methods but made their return values intances of objects without names

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Team < ActiveRecord::Base
     has_many :players
     has_many :user_teams
@@ -6,6 +7,12 @@ class Team < ActiveRecord::Base
     def find_team(team_name)
         team=Team.all.find_by(name:"#{team_name}")
         team
+    end
+
+    def list_players
+        list = Player.where(team_id:self.id)
+        #returns an array of the instances
+        list
     end
 
     
--- bin/run.rb
+++ bin/run.rb
@@ -3,7 +3,7 @@ require_relative "../app/models/player.rb"
 require_relative "../app/models/team.rb"
 require_relative "../app/models/user_team.rb"
 require_relative "../app/models/user.rb"
-# require_relative "./cli_methods.rb"
+require_relative "./cli_methods.rb"
 
 
 puts "Welcome to Sports CLI App"
@@ -13,4 +13,4 @@ puts "-------------------------"
 
 current_user = user_login
 
-
+User.first.list_favorite_teams
