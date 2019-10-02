d6cd9f43b05e6158d51441f7cca2f221d2fccecc
chungcha <charlieschung@gmail.com>
2019-10-02 14:08:43 -0400

    changed the api array to a global variable

--- app/models/api_communicator.rb
+++ app/models/api_communicator.rb
@@ -13,24 +13,19 @@ end
 
 
 def all_teams_hashes #only gets teams for 4 leagues
-    total_arr=[]
-    england_string=RestClient.get("https://api.football-data.org/v2/competitions/2021/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    england_string=RestClient.get("https://api.football-data.org/v2/competitions/2021/teams", {"X-Auth-Token"=> ""})
     england_arr=JSON.parse(england_string)["teams"]
-    spain_string=RestClient.get("https://api.football-data.org/v2/competitions/2014/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    spain_string=RestClient.get("https://api.football-data.org/v2/competitions/2014/teams", {"X-Auth-Token"=> ""})
     spain_arr=JSON.parse(spain_string)["teams"]
-    italy_string=RestClient.get("https://api.football-data.org/v2/competitions/2019/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    italy_string=RestClient.get("https://api.football-data.org/v2/competitions/2019/teams", {"X-Auth-Token"=> ""})
     italy_arr=JSON.parse(italy_string)["teams"]
-    german_string=RestClient.get("https://api.football-data.org/v2/competitions/2002/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    german_string=RestClient.get("https://api.football-data.org/v2/competitions/2002/teams", {"X-Auth-Token"=> ""})
     german_arr=JSON.parse(german_string)["teams"]
     $Total_arr = spain_arr + italy_arr + german_arr + england_arr
-    test
-    
 end
-
-
 
     def test
         binding.pry
     end
 
-    all_teams_hashes
+test
