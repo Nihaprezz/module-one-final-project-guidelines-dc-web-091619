93fa61404ec9fc726499418d492121ba7b04e55d
chungcha <charlieschung@gmail.com>
2019-10-03 14:15:45 -0400

    RSpec

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,14 +1,16 @@
-RSpec.describe Team do
+require_relative '../config/environment'
+require_relative "../app/models/player.rb"
+require_relative "../app/models/team.rb"
+require_relative "../app/models/user_team.rb"
+require_relative "../app/models/user.rb"
+require_relative "../bin/cli_methods.rb"
 
-    it "searches for the same team names" do
-        =Team.find_team("ARSENAL")
+RSpec.describe "Team" do
+
+    it "returns 108 if team_name is ARSENAL" do
+        expect(Team.find_team("ARSENAL FC")).to be(108)
     end
 
-
-
-
-
-
-
+    
 
 end
