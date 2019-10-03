e19d786c4098fe64c5c7b090f7b23678f3ce8899
nihaprezz <sergio13prez@gmail.com>
2019-10-03 17:18:44 -0400

    changed api communicator to stats

--- app/models/api_communicator.rb
+++ app/models/api_communicator.rb
@@ -14,13 +14,14 @@ $Total_arr = []
 
 
 def all_teams_hashes #only gets teams for 4 leagues
-    england_string=RestClient.get("https://api.football-data.org/v2/competitions/2021/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
-    england_arr=JSON.parse(england_string)["teams"]
-    spain_string=RestClient.get("https://api.football-data.org/v2/competitions/2014/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
-    spain_arr=JSON.parse(spain_string)["teams"]
-    italy_string=RestClient.get("https://api.football-data.org/v2/competitions/2019/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
-    italy_arr=JSON.parse(italy_string)["teams"]
-    german_string=RestClient.get("https://api.football-data.org/v2/competitions/2002/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
-    german_arr=JSON.parse(german_string)["teams"]
-    $Total_arr = spain_arr + italy_arr + german_arr + england_arr
+    england_string=RestClient.get("https://api.football-data.org/v2/competitions/2021/standings", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    england_stats=JSON.parse(england_string)['standings'][0]['table']
+    spain_string=RestClient.get("https://api.football-data.org/v2/competitions/2014/standings", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    spain_stats=JSON.parse(spain_string)['standings'][0]['table']
+    italy_string=RestClient.get("https://api.football-data.org/v2/competitions/2019/standings", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    italy_stats=JSON.parse(italy_string)['standings'][0]['table']
+    german_string=RestClient.get("https://api.football-data.org/v2/competitions/2002/standings", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
+    german_stats=JSON.parse(german_string)['standings'][0]['table']
+    $Total_arr = spain_stats + italy_stats + german_stats + england_stats
+    binding.pry
 end
