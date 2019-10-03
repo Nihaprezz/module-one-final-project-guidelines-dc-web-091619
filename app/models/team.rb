6adc94720547d5726476d9f80ce2a73e57a1e993
chungcha <charlieschung@gmail.com>
2019-10-02 20:25:33 -0400

    worked on match method

--- app/models/team.rb
+++ app/models/team.rb
@@ -19,10 +19,31 @@ class Team < ActiveRecord::Base
     def matches
         week_behind=(Date.today-7).strftime('%Y-%m-%d')
         week_forward=(Date.today+7).strftime('%Y-%m-%d')
-        match_string=RestClient.get("https://api.football-data.org/v2/teams/1044/matches?dateFrom=#{week_behind}&dateTo=#{week_forward}", {"X-Auth-Token"=> "######TOP SECRET#########"})
+        match_string=RestClient.get("https://api.football-data.org/v2/teams/#{self.team_api_id}/matches?dateFrom=#{week_behind}&dateTo=#{week_forward}", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
         match_hash=JSON.parse(match_string)
-        @match_array = match_hash["matches"]
+        match_array = match_hash["matches"]
+        match_array.each do |match|
+            if match["status"] == "FINISHED"
+                puts ""
+                puts "Past Fixtures"
+                puts "----------"
+                puts "#{match["homeTeam"]["name"]}: #{match["score"]["fullTime"]["homeTeam"]}"
+                puts "#{match["awayTeam"]["name"]}: #{match["score"]["fullTime"]["awayTeam"]}"
+                binding.pry
+            elsif match["status"] == "SCHEDULED"
+                puts ""
+                puts ""
+                puts "Upcoming Fixtures"
+                puts "-----------------"
+                # puts "#{match["utcDate"]}"
+                #This doesnt work.
+                puts "#{match["homeTeam"]["name"]}"
+                puts "#{match["awayTeam"]["name"]}"
+                puts ""
+            end
+        end
     end
+    binding.pry 
 
     # def stats
 
