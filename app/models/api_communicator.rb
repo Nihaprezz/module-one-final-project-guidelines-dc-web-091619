0e647d676619a50b0d3b6c5a1eeeed5bbe43b6be
chungcha <charlieschung@gmail.com>
2019-10-03 12:28:35 -0400

    made the most_popular_team method

--- app/models/api_communicator.rb
+++ app/models/api_communicator.rb
@@ -16,11 +16,11 @@ $Total_arr = []
 def all_teams_hashes #only gets teams for 4 leagues
     england_string=RestClient.get("https://api.football-data.org/v2/competitions/2021/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
     england_arr=JSON.parse(england_string)["teams"]
-    spain_string=RestClient.get("https://api.football-data.org/v2/competitions/2014/teams", {"X-Auth-Token"=> ""})
+    spain_string=RestClient.get("https://api.football-data.org/v2/competitions/2014/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
     spain_arr=JSON.parse(spain_string)["teams"]
-    italy_string=RestClient.get("https://api.football-data.org/v2/competitions/2019/teams", {"X-Auth-Token"=> ""})
+    italy_string=RestClient.get("https://api.football-data.org/v2/competitions/2019/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
     italy_arr=JSON.parse(italy_string)["teams"]
-    german_string=RestClient.get("https://api.football-data.org/v2/competitions/2002/teams", {"X-Auth-Token"= ""> "ebf9f744f51940048af126de1c5c27b7"})
+    german_string=RestClient.get("https://api.football-data.org/v2/competitions/2002/teams", {"X-Auth-Token"=> "ebf9f744f51940048af126de1c5c27b7"})
     german_arr=JSON.parse(german_string)["teams"]
     $Total_arr = spain_arr + italy_arr + german_arr + england_arr
 end
--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -3,5 +3,17 @@ class User_team < ActiveRecord::Base
     belongs_to :team
     has_many :players, through: :team
 
+    def self.team_count_hash
+        count_hash = {}
+        self.all.each do |user_team|
+            count_hash[user_team.team_id] ? count_hash[user_team.team_id]+=1 : count_hash[user_team.team_id]=1
+        end
+        count_hash
+    end
+
+    def self.most_popular_team
+        most_pop=self.team_count_hash.max_by{|id,num|num}
+        prints "#{Team.find_by(id:most_pop[0]).name}"
+    end
 
 end
